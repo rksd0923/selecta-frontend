99fe0dd5578337b47699d0a6404c104f43d41f4d
Ruben Rivera <rerivera76@gmail.com>
2020-12-18 00:39:57 -0500

    more CSS updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,18 @@ const Navbar = () => {
     return (
 
 
-        <ReactBootstrap.Navbar bg="light" expand="lg">
-            <ReactBootstrap.Navbar.Brand href="#home">Selecta por Siempre</ReactBootstrap.Navbar.Brand>
+        <ReactBootstrap.Navbar bg="secondary" variant="dark" expand="lg">
+            <ReactBootstrap.Navbar.Brand className="navbar" href="#home">Selecta por Siempre</ReactBootstrap.Navbar.Brand>
             <ReactBootstrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
             <ReactBootstrap.Navbar.Collapse id="basic-navbar-nav">
                 <ReactBootstrap.Nav className="mr-auto">
                     <Link to="/players">
-                        <ReactBootstrap.Nav.Link href="#Jugadores">Equipo</ReactBootstrap.Nav.Link>
+                        <ReactBootstrap.Nav.Link href="#Equipo">Equipo</ReactBootstrap.Nav.Link>
                     </Link>
-                    <ReactBootstrap.Nav.Link href="#link">Entrenador</ReactBootstrap.Nav.Link>
+
 
                 </ReactBootstrap.Nav>
-                <ReactBootstrap.Form inline>
-                    <ReactBootstrap.Nav.Link href="#home">REGISTRATE</ReactBootstrap.Nav.Link>
-                    <ReactBootstrap.Nav.Link href="#home">Entra</ReactBootstrap.Nav.Link>
-                    <ReactBootstrap.Nav.Link href="#home">Cierra Sesion</ReactBootstrap.Nav.Link>
-                </ReactBootstrap.Form>
+
             </ReactBootstrap.Navbar.Collapse>
         </ReactBootstrap.Navbar >
 
